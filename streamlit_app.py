3f4eaba515f24241d117e4f1c2f9d3b7d0fa7c52
Rajatkpaliwal <rajatkpaliwal05@gmail.com>
2026-05-14 08:39:53 +0530

    Resolved issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_classic.chains import LLMMathChain, LLMChain
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.agents.agent_types import AgentType
 from langchain.agents import Tool, initialize_agent
